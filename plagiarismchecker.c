12ffd779efe0fbc202d2845c74fa126a173982e4
Mohamoud <mohamoud@vdi-scclab-101.lancs.local>
2023-05-19 16:07:36 +0100

    this function calculates the frequency of phrases that occur within both token arrays

--- plagiarismchecker.c
+++ plagiarismchecker.c
@@ -86,3 +86,41 @@ float compare_files(const char *test_file1, const char *test_file2) {
     float similarity = ((float)count / (float)(num_tokens1 + num_tokens2 - count)) * 100.0f;
     return similarity;
 }
+
+
+void calculate_phrase_frequency(char tokens1[][100], int num_tokens1, char tokens2[][100], int num_tokens2, char phrases[][100], int max_phrases, int threshold, int *phrase_frequency) {
+   char phrase[200];
+   int num_consecutive_words = 0;
+   int num_phrases = 0;
+   phrase[0] = '\0';
+   
+   for (int i = 0; i < num_tokens1; i++) {
+       int match = 0; 
+       for (int j = 0; j < num_tokens2; j++) {
+           if (strncmp(tokens1[i], tokens2[j], 100) == 0) {
+               match = 1; 
+               num_consecutive_words++;
+               if (num_consecutive_words == 1) { 
+                   strncpy(phrase, tokens1[i], 100);
+               } else {
+                   strncat(phrase, "", 1);
+                   strncat(phrase, tokens1[i], 100);
+               }
+               if (num_consecutive_words >= threshold) {
+                   strncpy(phrases[num_phrases], phrase, 100);
+                   phrase_frequency[num_phrases] = num_consecutive_words;
+                   num_phrases++;
+                   if (num_phrases >= max_phrases) {
+                       return;
+                   }
+               }
+               break;
+           }
+       }
+       
+       if (!match) {
+           num_consecutive_words = 0;
+           phrase[0] = '\0';
+       }
+   }
+}
