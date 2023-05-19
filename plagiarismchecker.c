de0fff7b2ffdc75a40772a2e33205b5121b42e3d
Mohamoud <mohamoud@vdi-scclab-101.lancs.local>
2023-05-19 14:58:35 +0100

    the update i have made is that i have created a struct function as it will allow me to hold a word and its frequency

--- plagiarismchecker.c
+++ plagiarismchecker.c
@@ -7,6 +7,11 @@
 #define MAX_FILE_SIZE 100000
 #define MAX_TOKEN_LENGTH 100
 
+
+struct word_frequency {
+    char word[MAX_TOKEN_LENGTH];
+    int frequency;
+};
 char *read_file(const char *filename) {
  FILE *fp;
    char *buffer = NULL;
